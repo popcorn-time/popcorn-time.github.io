5625df60c23a4c23af8ada3e50785ac00990147f
Patineta <patineta08@gmail.com>
2014-03-08 19:16:59 -0300

    Fixed the responsive thing.

--- js/site.js
+++ js/site.js
@@ -1,16 +1,5 @@
-// Disable WOW on small screens.
-if($(window).width() > 768) {
-  wow = new WOW(
-    {
-      animateClass: 'animated',
-      offset:       100
-    }
-  );
-  wow.init();
-};
-
 var userAgent = navigator.platform;
-//alert(userAgent);
+
 if(userAgent.substring(0,3) == "Mac") {
 	document.getElementsByTagName('body')[0].className+=' mac';
 } else if (userAgent.substring(0,3) == "Win") {
@@ -21,9 +10,17 @@ if(userAgent.substring(0,3) == "Mac") {
 	document.getElementsByTagName('body')[0].className+=' nope';
 }
 
-/*var obj = document.getElementById("header");
-var topVal = parseInt(obj.style.top, 10);
-obj.style.top = (topVal + 300) + "px";*/
-
 var heroHeight = document.getElementById('header').offsetHeight;
-//alert(heroHeight);
+
+// Disable WOW on small screens.
+try {
+  if(window.innerWidth > 768) {
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100
+      }
+    );
+    wow.init();
+  };
+} catch(err) {  }
